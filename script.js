addad4f2d053c782d985311b83313ef9ec002e35
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-08-15 17:50:25 -0500

    working on menu grid

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-let button = document.createElement("button");
+// let button = document.createElement("button");
 
-let btn = document.getElementById("button");
+// let btn = document.getElementById("button");
 
 const slideMenu = document.getElementById("slide-menu");
 
-button.innerText = "Click for Menu";
-
-btn.append(button);
-
 button.addEventListener("click", function() {
   slideMenu.classList.toggle("active");
-  if (slideMenu.classList == "slide-menu active") {
-    this.style.backgroundColor = "yellow";
-  } else {
-    this.style.backgroundColor = "";
-  }
+  // if (slideMenu.classList == "slide-menu active") {
+  //   this.style.backgroundColor = "yellow";
+  // } else {
+  //   this.style.backgroundColor = "";
+  // }
 });
