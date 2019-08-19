0ea1f07136c612d5574f5629d583488e88b7231c
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-08-19 09:43:59 -0500

    added blur on menu click

--- script.js
+++ script.js
@@ -4,9 +4,15 @@
 
 const slideMenu = document.getElementById("slide-menu");
 const button = document.getElementById("button");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
 
 button.addEventListener("click", function() {
   slideMenu.classList.toggle("active");
+  first.style = "filter: blur(4px)";
+  second.style = "filter: blur(4px)";
+  third.style = "filter: blur(4px)";
   // if (slideMenu.classList == "slide-menu active") {
   //   this.style.backgroundColor = "yellow";
   // } else {
