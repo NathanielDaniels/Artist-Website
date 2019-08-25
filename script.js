13cd829ad1ef4a6db9d325764c939c5b245ad51e
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-08-25 09:17:37 -0500

    added to animations

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+//?===============================
+//* DOM
 const slideMenu = document.getElementById("slideMenu");
-const button = document.getElementById("button");
+const menuButton = document.getElementById("button");
 const first = document.getElementById("first");
 const second = document.getElementById("second");
 const third = document.getElementById("third");
 const menuClose = document.getElementById("close-btn");
 
-button.addEventListener("click", function() {
+//?===============================
+//* GSAP Animations
+const $modelLeft = document.getElementById("modelLeft");
+const $modelRight = document.getElementById("modelRight");
+const $pageMenu = document.getElementById("pageMenu");
+const $userInfo = document.getElementById("userInfo");
+const $mainTitle = document.getElementById("title");
+
+TweenLite.from($modelLeft, 1.2, { y: 900, opacity: 0 });
+TweenLite.from($modelRight, 1.2, { y: -900, opacity: 0 });
+TweenLite.from($userInfo, 1, { delay: 1.4, x: 300, opacity: 0 });
+TweenLite.from($pageMenu, 1, { delay: 1.4, x: -300, opacity: 0 });
+TweenLite.from($mainTitle, 1, { delay: 2.2, x: -300, opacity: 0 });
+TweenLite.from(menuButton, 1, { delay: 2.2, x: 300, opacity: 0 });
+TweenLite.from(first, 1, { delay: 0.3, opacity: 0 });
+
+//?===============================
+//* Menu Button Click
+
+menuButton.addEventListener("click", function() {
   slideMenu.classList.toggle("active");
   first.style = "filter: blur(4px)";
   second.style = "filter: blur(4px)";
@@ -18,18 +39,6 @@ menuClose.addEventListener("click", function() {
   second.style = "filter: blur(0)";
   third.style = "filter: blur(0)";
 });
-
-//?===============================
-//* GSAP Animations
-const $modelLeft = document.getElementById("modelLeft");
-const $modelRight = document.getElementById("modelRight");
-const $pageMenu = document.getElementById("pageMenu");
-const $userInfo = document.getElementById("userInfo");
-
-TweenLite.from($modelLeft, 1.2, { y: 900, opacity: 0 });
-TweenLite.from($modelRight, 1.2, { y: -900, opacity: 0 });
-TweenLite.from($userInfo, 1, { delay: 1.4, x: 300, opacity: 0 });
-TweenLite.from($pageMenu, 1, { delay: 1.4, x: -300, opacity: 0 });
 
 //?===============================
 
