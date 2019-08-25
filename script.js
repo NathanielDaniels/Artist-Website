4eabd5b47f056479fe15627c40b193b61428ce10
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-08-25 12:12:37 -0500

    working on menu button issues

--- script.js
+++ script.js
@@ -6,22 +6,46 @@ const first = document.getElementById("first");
 const second = document.getElementById("second");
 const third = document.getElementById("third");
 const menuClose = document.getElementById("close-btn");
-
-//?===============================
-//* GSAP Animations
 const $modelLeft = document.getElementById("modelLeft");
 const $modelRight = document.getElementById("modelRight");
 const $pageMenu = document.getElementById("pageMenu");
 const $userInfo = document.getElementById("userInfo");
 const $mainTitle = document.getElementById("title");
 
-TweenLite.from($modelLeft, 1.2, { y: 900, opacity: 0 });
-TweenLite.from($modelRight, 1.2, { y: -900, opacity: 0 });
-TweenLite.from($userInfo, 1, { delay: 1.4, x: 300, opacity: 0 });
-TweenLite.from($pageMenu, 1, { delay: 1.4, x: -300, opacity: 0 });
-TweenLite.from($mainTitle, 1, { delay: 2.2, x: -300, opacity: 0 });
-TweenLite.from(menuButton, 1, { delay: 2.2, x: 300, opacity: 0 });
-TweenLite.from(first, 1, { delay: 0.3, opacity: 0 });
+//?===============================
+//* GSAP Animations
+
+TweenLite.from($modelLeft, 1.2, { y: 900, opacity: 0, ease: Power2.easeInOut });
+TweenLite.from($modelRight, 1.2, {
+  y: -900,
+  opacity: 0,
+  ease: Power1.easeInOut
+});
+TweenLite.from($userInfo, 1, {
+  delay: 1.1,
+  x: 300,
+  opacity: 0,
+  ease: Power2.easeInOut
+});
+TweenLite.from($pageMenu, 1, {
+  delay: 1.1,
+  x: -300,
+  opacity: 0,
+  ease: Power2.easeInOut
+});
+TweenLite.from($mainTitle, 1, {
+  delay: 1.7,
+  x: -300,
+  opacity: 0,
+  ease: Power2.easeInOut
+});
+TweenLite.from(menuButton, 1, {
+  delay: 1.7,
+  x: 300,
+  opacity: 0,
+  ease: Power2.easeInOut
+});
+TweenLite.from(first, 1, { delay: 0.3, opacity: 0, ease: Power2.easeInOut });
 
 //?===============================
 //* Menu Button Click
@@ -53,7 +77,7 @@ menuClose.addEventListener("click", function() {
 //   // first.style = "filter: blur(0px)";
 // });
 
-//========================================
+//?========================================
 
 // const colors = ["red", "yellow", "blue", "green"];
 
