74703ba28779101730ec9b8353cecbf3cda48295
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-08-29 23:55:40 -0500

    removing leftover code

--- script.js
+++ script.js
@@ -65,16 +65,3 @@ menuClose.addEventListener("click", function() {
 });
 
 //?===============================
-
-//*! target document
-// // Detect all clicks on the document
-// document.addEventListener("click", function(event) {
-//   // If user clicks inside the element, do nothing
-//   if (event.target.closest(".slide-menu")) return;
-
-//   // If user clicks outside the element, hide it!
-//   slideMenu.classList.add(".new");
-//   // first.style = "filter: blur(0px)";
-// });
-
-//?========================================
