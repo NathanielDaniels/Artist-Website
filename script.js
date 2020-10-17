ddd154950317a683011835e55dd90cd0ea7f142d
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-10-16 20:30:05 -0500

    trying to add page transition using GSAP. This might take a bit

--- script.js
+++ script.js
@@ -13,7 +13,37 @@
 // const $mainTitle = document.getElementById("title");
 
 //?===============================
-//* GSAP Animations
+//* GSAP Page Change Animation
+
+const $secondArtist = document.querySelector("#secondArtist")
+const $logo = document.querySelector('.transition__logo');
+const $frameBlack = document.querySelector('.page-transition__black');
+const $frameRed = document.querySelector('.page-transition__red');
+const $button = document.querySelectorAll('.button');
+
+// console.log($frameBlack, $frameRed)
+
+let tltransition = new TimelineMax({paused:true})
+  .fromTo($frameRed , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},)
+  .fromTo($frameBlack , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},.2)
+  .fromTo($logo , 1.6, {xPercent: -100, autoAlpha:0 },{xPercent: 0, autoAlpha:1, ease: Power4.easeInOut},.7)
+  .set($frameRed, {scaleX:0})
+  .set($secondArtist, {autoAlpha:0})
+  .to($frameBlack , 2.2, {scaleX: 0, transformOrigin:'right', ease: Power4.easeInOut})
+  .to($logo , .2, {autoAlpha:0 },'-=1.2')
+  
+
+$button.forEach(button => buttonClick(button))
+
+function buttonClick(button) {
+  button.addEventListener('click', () => {
+    console.log("button clicked")
+    tltransition.play(0);
+});
+}
+
+//?===============================
+//* GSAP Page Item Animations
 const $modelLeft = document.getElementById("modelLeft");
 const $modelRight = document.getElementById("modelRight");
 const $pageMenu = document.getElementById("pageMenu");
